4662f67c0f8e42cc9d37789841f81a181525357c
Shreyanshu Bhushan <33536680+shreyanshu09@users.noreply.github.com>
2020-09-18 16:11:22 +0530

    Add files via upload

---
+++ 1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shreyanshu Bhushan PHP</title>
+</head>
+<body>
+<h1>Shreyanshu Bhushan PHP</h1>
+<p>The SHA256 hash of "Shreyanshu Bhushan" is
+    <?php print hash('sha256', 'Shreyanshu Bhushan');?></p>
+<pre>ASCII ART:
+
+    ***********
+    **       **
+    **
+    ***********
+             **
+    **       **
+    ***********
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
